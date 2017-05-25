f0cb388740d63d4ae26621906634f68cf011f82c
wolfreezark <oxneedless@gmail.com>
2017-05-25 15:07:25 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/coolstar/theos.git && mkdir -p theos/toolchain/windows
+git clone -b x86_64 https://github.com/coolstar/iOSToolchain4Win.git theos/toolchain/windows/iphone
+mkdir theos/sdks
+cd theos/sdks
+wget https://sdks.website/dl/iPhoneOS9.3.sdk.tbz2
+tar xvf iPhoneOS9.3.sdk.tbz2
