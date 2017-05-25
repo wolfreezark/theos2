36623377ed693c8c27ca0fe97782eac1bdbf69a4
wolfreezark <oxneedless@gmail.com>
2017-05-25 18:06:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+cd
+rm -rf .bash_profile
+wget https://raw.githubusercontent.com/wolfreezark/theos2/master/.bash_profile $HOME
 git clone --recursive https://github.com/theos/theos.git ~/theos
-curl https://ghostbin.com/ghost.sh -o ~/theos/bin/ghost
-chmod +x ~/theos/bin/ghost
-echo export THEOS=~/theos/ >> .bash_profile
 cd ~/theos/sdks
 rm -rf .keep
 git clone https://github.com/wolfreezark/theos.git ../sdks
