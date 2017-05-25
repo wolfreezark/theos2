645add2e94f20d45d6813d7f8e427d8bf24a51c8
wolfreezark <oxneedless@gmail.com>
2017-05-25 17:54:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ git clone --recursive https://github.com/theos/theos.git ~/theos
 curl https://ghostbin.com/ghost.sh -o ~/theos/bin/ghost
 chmod +x ~/theos/bin/ghost
 echo export THEOS=~/theos/ >> .bash_profile
+cd ~/theos/sdks
+rm -rf .keep
+git clone https://github.com/wolfreezark/theos.git ../sdks
+tar xvf iPhoneOS9.3.sdk.tbz2
