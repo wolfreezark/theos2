808c22f2a2bda83854a7f14067cfff3356fc2055
wolfreezark <oxneedless@gmail.com>
2017-05-25 16:57:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd /opt
 git clone -b windows git://github.com/coolstar/theos.git
 git clone -b master git://github.com/coolstar/iOSToolchain4Win.git theos/toolchain/windows/iphone
 git clone https://github.com/wolfreezark/theos.git /opt/theos/sdks
-tar xvf opt/theos/sdks/iPhoneOS9.3.sdk.tbz2
+tar xvf /opt/theos/sdks/iPhoneOS9.3.sdk.tbz2
 cd
 mkdir projects
 cd projects
